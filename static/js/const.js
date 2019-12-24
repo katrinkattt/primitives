a426d41f3defd4985841c8eaff5a92304e104f38
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-24 21:29:01 +0500

    Create const.js

---
+++ static/js/const.js
@@ -0,0 +1,11 @@
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const loading = document.getElementById('loading');
+const download = document.getElementById('download');
+const ready = document.getElementById('ready');
+const dlinn = $('#loading').innerWidth();
+
+const time = document.getElementById('time');
+
+const block = document.getElementById("block");
+const newBlock = document.getElementById('newBlock');
